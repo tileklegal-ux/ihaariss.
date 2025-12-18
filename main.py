001bd388bee52df291f1e1940f033bb425ff7469
tileklegal-ux <tileklegal@gmail.com>
2025-12-18 21:59:43 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,17 @@
-import logging
-from telegram.ext import (
-    ApplicationBuilder,
-    CommandHandler,
-    MessageHandler,
-    CallbackQueryHandler,
-    filters,
-)
+from telegram.ext import Application
 
-from config import BOT_TOKEN
-from handlers.owner import owner_start, register_handlers_owner
-from handlers.user import handle_user_message
-from handlers.role_actions import role_text_router
-from database.db import ensure_user_exists
-
-
-logging.basicConfig(level=logging.INFO)
-
-
-async def start(update, context):
-    user = update.effective_user
-    ensure_user_exists(user.id, user.username)
-    await update.message.reply_text("👋 Добро пожаловать в ArtBazar AI!")
+from handlers.owner import register_handlers_owner
+from database.db import init_db
+from user import register_user_handlers  # твой user.py
 
 
 def main():
-    app = ApplicationBuilder().token(BOT_TOKEN).build()
+    init_db()
 
-    app.add_handler(CommandHandler("start", start))
+    app = Application.builder().token("BOT_TOKEN").build()
 
+    register_user_handlers(app)
     register_handlers_owner(app)
-
-    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, role_text_router))
-    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_user_message))
 
     app.run_polling()
 
