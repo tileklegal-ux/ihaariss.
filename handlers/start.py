ca4e9f96cfcf065a443c74f8e835d317e8b9d560
tileklegal-ux <tileklegal@gmail.com>
2025-12-18 22:43:00 +0600

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,29 +1,29 @@
-# handlers/start.py
 from telegram import Update
-from telegram.ext import ContextTypes, CommandHandler, Application
+from telegram.ext import ContextTypes, CommandHandler
 
-from database.db import get_user_role, ensure_user_exists
+from database.db import get_user_role
 from handlers.owner import owner_start
 from handlers.manager import manager_start
-from handlers.user import cmd_start_user
+from handlers.user import user_start
 
 
 async def start_router(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    user = update.effective_user
-    ensure_user_exists(user.id, user.username)
+    user_id = update.effective_user.id
+    role = get_user_role(user_id)
 
-    role = get_user_role(user.id)
-
+    # OWNER — самый высокий приоритет
     if role == "owner":
         await owner_start(update, context)
         return
 
+    # MANAGER
     if role == "manager":
         await manager_start(update, context)
         return
 
-    await cmd_start_user(update, context)
+    # USER (по умолчанию)
+    await user_start(update, context)
 
 
-def register_start_handlers(app: Application):
-    app.add_handler(CommandHandler("start", start_router))
+def register_start_handlers(application):
+    application.add_handler(CommandHandler("start", start_router), group=0)
